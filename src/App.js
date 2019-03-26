16ddd6887613e53c391b0913ac9ca1c182d2a053
Charley McGowan <glitchwizard@gmail.com>
2019-03-26 13:28:00 -0700

    commit to transfer work to desktop.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, {
-  Component
-} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
 import stargasmLogo from '../src/assets/2018-02-18---Stargasm-logo2_02.png';
@@ -9,13 +7,87 @@ import YouTube from 'react-youtube';
 import unMuteButton from './assets/Speaker_Icon.svg';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      volume: 0.2,
+      paused: false,
+      mute: true
+    };
+    
+    this.toggleMute = this.toggleMute.bind(this);
+    this.handlePause = this.handlePause.bind(this);
+    this.handlePlayerPause = this.handlePlayerPause.bind(this);
+    this.handlePlayerPlay = this.handlePlayerPlay.bind(this);
+    this.handleVolume = this.handleVolume.bind(this);
+  }
+
+  handlePause(event) {
+    this.setState({
+      paused: event.target.checked,
+    });
+  }
+
+  handlePlayerPause() {
+    this.setState({paused: true});
+  }
+
+  handlePlayerPlay() {
+    this.setState({paused: false});
+  }
+
+  handleVolume(event) {
+    this.setState({
+      volume: parseFloat(event.target.value),
+    });
+  }
+
+  handleVolumeUp(event){
+    let currentVolume = parseFloat(this.state.volume);
+    if (currentVolume < 1) {
+      currentVolume += .10
+      this.setState({
+        volume: currentVolume
+      })
+    }
+    
+  }
+
+  handleVolumeDown(event){
+    let currentVolume = parseFloat(this.state.volume);
+    if (currentVolume < 1) {
+      currentVolume += .10
+      this.setState({
+        volume: currentVolume
+      })
+    }
+  }
+ 
 
 _onReady(event){
   event.target.mute();
+
+}
+
+toggleMute(event){
+  alert('event' + event.target);
+  console.log('event: \n' + event);
+
+ if(event.target.isMuted()){
+   event.target.unMute();
+ } else {
+   event.target.mute();
+ }
 }
 
 
   render() {
+
+    const {
+      volume, paused, suggestedQuality,
+    } = this.state;
+
     const youTubeOptions = {
       height: '100%',
       width: '100%',
@@ -23,12 +95,13 @@ _onReady(event){
         autoplay: 1,
         controls: 0,
         loop: 1,
-        enablejsapi: 1
+        enablejsapi: 1,
+        iv_load_policy: 3
       }
-      
     }
 
-    return (<div className="App" >
+    return (
+    <div className="App" >
         <div className="vid" >
             <YouTube
               videoId='lpMESlFzTx0'
@@ -37,11 +110,17 @@ _onReady(event){
               opts={youTubeOptions}
               onReady={this._onReady}
             />
+      </div>
+        <div className="headerBar">
+          <div>
+            Volume controls will go here
+          </div>
+          <div className="unMuteButtonContainer" onClick={this.toggleMute}>
+            <img src={unMuteButton} alt='Unmute Button' id="unMuteButtonIcon"/>
+          </div>
         </div>
+          
       <div className="App-header" >
-        <div className="unMuteButtonContainer">
-          <img src={unMuteButton} alt='Unmute Button' id="unMuteButtonIcon"/>
-        </div>
         <div className='AppText'>
           <div className='callToAction'> LISTEN TO </div>
           <img src={stargasmLogo}
