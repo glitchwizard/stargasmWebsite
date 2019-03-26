f894045c7f0ff389b07609a6196251607ca47b86
charley mcgowan <charley.mcgowan@outlook.com>
2019-03-26 16:47:50 -0700

    working on new button interfaces

--- src/App.js
+++ src/App.js
@@ -112,7 +112,6 @@ class App extends Component {
             alt="logo" />
         <div className="headerBar">
           <div className="headerItem">
-              
              <input
                 type="range"
                 value={volume}
