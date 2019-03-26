f4201af741575592b8430fd52115255978bde855
Charley McGowan <glitchwizard@gmail.com>
2019-03-25 21:26:47 -0700

    switch over to react-youtube component, also add unmute button

--- src/App.js
+++ src/App.js
@@ -5,48 +5,69 @@ import './App.css';
 import Particles from 'react-particles-js';
 import stargasmLogo from '../src/assets/2018-02-18---Stargasm-logo2_02.png';
 import particleConfig from './assets/particlesjs-config.json';
+import YouTube from 'react-youtube';
+import unMuteButton from './assets/Speaker_Icon.svg';
 
 class App extends Component {
+
+_onReady(event){
+  event.target.mute();
+}
+
+
   render() {
+    const youTubeOptions = {
+      height: '100%',
+      width: '100%',
+      playerVars: {
+        autoplay: 1,
+        controls: 0,
+        loop: 1,
+        enablejsapi: 1
+      }
+      
+    }
+
     return (<div className="App" >
-      <div className="App-header" >
         <div className="vid" >
-          <iframe 
-            title="stargasmYoutubeVid"
-            width="100%"
-            height="100%"
-            src="https://www.youtube.com/embed/lpMESlFzTx0?controls=0;autoplay=1"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen >
-          </iframe>
+            <YouTube
+              videoId='lpMESlFzTx0'
+              className="youtubePlayerVidep"
+              containerClassName='youtubePlayerContainer'
+              opts={youTubeOptions}
+              onReady={this._onReady}
+            />
+        </div>
+      <div className="App-header" >
+        <div className="unMuteButtonContainer">
+          <img src={unMuteButton} alt='Unmute Button' id="unMuteButtonIcon"/>
         </div>
         <div className='AppText'>
-          <div className='callToAction'> LISTEN TO </div> 
+          <div className='callToAction'> LISTEN TO </div>
           <img src={stargasmLogo}
-              className="App-logo"
-              alt="logo" />
+            className="App-logo"
+            alt="logo" />
         </div>
-      </div> 
-      <Particles 
-          height="100vh"
-          width="100vw"
-          params={
-            particleConfig
+      </div>
+      <Particles
+        height="100vh"
+        width="100vw"
+        params={
+          particleConfig
+        }
+        style={
+          {
+            position: 'fixed',
+            top: '0px',
+            margin: '0',
+            padding: '0',
+            zIndex: '-1',
           }
-          style={
-            {
-              position: 'fixed',
-              top: '0px',
-              margin: '0',
-              padding: '0',
-              zIndex: '-1',
-            }
 
-          }
-        /> </div>
-      );
-    }
+        }
+      /> </div>
+    );
   }
-  
+}
+
 export default App;
